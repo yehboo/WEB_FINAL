c529ecdf92c9b12189456fe1393f99b78e39f5d5
unknown <bob40711@gmail.com>
2021-06-25 22:18:42 +0800

    first commit

---
+++ ytplayer/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "rVv-WBulc8s",
+    "UTtZaFYL7Bc",
+    "49u9WQJnv3o"
+];
+    //播放起訖秒數
+    let playTime = [
+        [137,145],
+        [24,45],
+        [0, 14]
+];
